9828d8ce7bb83d63e4d939beb0bc945774623550
Oghenetega Esedere <arcteggzz@gmail.com>
2022-11-18 19:17:13 +0000

    feat: added right sidebar components

--- src/pages/Transcribe/Components/TranscriptionMain/TranscriptionRightBar/TranscriptionRightBar.jsx
+++ src/pages/Transcribe/Components/TranscriptionMain/TranscriptionRightBar/TranscriptionRightBar.jsx
@@ -1,9 +1,12 @@
 import styles from './TranscriptionRightBar.module.scss';
+import AudioControl from './Components/AudioControl'
+import SentimentAnalysis from './Components/SentimentAnalysis'
 
 function TranscriptionRightBar() {
   return (
     <div className={styles.TranscriptionRightBar}>
-      Tega
+      <AudioControl />
+      <SentimentAnalysis />
     </div>
   );
 }
